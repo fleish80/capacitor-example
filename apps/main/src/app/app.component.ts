4f7a1066857ce59d16349339bf31005e957b0735
DXF0530 <dmitry.fleish@sbdinc.com>
2022-10-20 15:02:51 +0300

    Initial commit

---
+++ apps/main/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'capacitor-example-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'main';
+}
